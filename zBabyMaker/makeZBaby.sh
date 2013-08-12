a582e766990cbbe37fd9d9e4a365401d3232695d
cwelke <cwelke@cern.ch>
2013-08-12 05:34:10 -0700

    updating paths

--- zBabyMaker/makeZBaby.sh
+++ zBabyMaker/makeZBaby.sh
@@ -2,11 +2,22 @@ function processsample
 {
 dataset=$1
 isdata=$2
-percentage=$3
-versionnumber=$4
+versionnumber=$3
+percentage=$4
+
+if [ ! -d log/${versionnumber} ]; then
+	echo "log/${versionnumber} does not exist. Attempting to create."
+	mkdir -p log/${versionnumber}
+	didMakeDir=$?
+else
+	echo "log/${versionnumber} Already exists! Output located in log/${versionnumber}/$dataset.txt"
+fi
+
+if [ "$didMakeDir" == "0" ]; then
+	echo "log/${versionnumber} created successfully.Output located in log/${versionnumber}/$dataset.txt"
+fi
 
 echo "Processing root -b -q \"doAllZ.C (\"$dataset\", \"$percentage\", $isdata)\""
-echo "Output located in log/$dataset.txt"
 root -b -q "doAllZ.C (\"$dataset\", \"$percentage\", $isdata)" > log/${versionnumber}/${dataset}.txt 2>&1 &
 sleep 5
 }
