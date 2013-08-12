38c839e00d2acea009d21505e8dacff3679054ae
cwelke <cwelke@cern.ch>
2013-08-12 03:50:14 -0700

    adding looper

---
+++ looper/makeAllZBabies.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+. makeZBaby.sh
+
+# processsample zjetsmc_ee false
+# processsample zjetsmc_mm false
+
+# processsample z1jet_53X_slim_ false
+# processsample z2jet_53X_slim_ false
+# processsample z3jet_53X_slim_ false
+# processsample z4jet_53X_slim_ false
+
+# processsample zjets_53X_slim false
+
+# processsample data_53X_2012A true
+# processsample data_53X_2012B true
+# processsample data_53X_2012C true
+# processsample data_53X_2012D true
+
+# processsample t_53X_slim false
+
+# processsample ttbar_mgdl_53X_slim false
+# processsample ttbar_mgsl_53X_slim false
+# processsample ttbar_mghad_53X_slim false
+
+# processsample TChizh_53X_150_1 false
+# processsample TChizh_53X_200_1 false
+# processsample TChizh_53X_250_1 false
+
+# processsample zhsm_53X_slim false
+
+# processsample zz2l2nu_53X_slim false
+# processsample zz2l2q_53X_slim false
+# processsample zz4l_53X_slim false
+
+# processsample wz2l2q_53X_slim false
+# processsample wz3lnu_53X_slim false
+
+# processsample ww_53X_slim false
+
+# processsample ttz_53X_slim false
+# processsample ttw_53X_slim false
+# processsample ttg_53X_slim false
+# processsample ttww_53X_slim false
+# processsample tbz_53X_slim false
+
+# processsample vvv_53X_slim false
+
+# processsample zbb2l_53X_slim false
+
